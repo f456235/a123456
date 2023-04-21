a48df2783b98f380784c112ff41a10aa301c8ee2
110062109 <xxxxx78918@gmail.com>
2023-04-21 20:05:16 +0800

    finish profile upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/home";
 function App() {
   return (
     <div>
-      <Home/>
+      <Register/>
     </div>
   );
 }
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import AddImage from '../img/add-image.png'
-
+import {createUserWithEmailAndPassword,updateProfile} from "firebase/auth";
+import {auth,storage,db} from  "../firebase";
+import { UserCredential } from 'firebase/auth';
+import {  ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 //create html objects
 /*var outerdiv = document.createElement('div');
 var innerdiv = document.createElement('div');
@@ -46,22 +51,84 @@ form.appendChild(file);
 console.log(outerdiv);  */
 
 const Register = () =>{
+    const [error,setError] = useState(false);
+    const SumbitEvent = async (e) =>{
+        e.preventDefault();
+        const userName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const userFile = e.target[3].files[0];
+        
+        try{
+            const result = await createUserWithEmailAndPassword (auth,email,password);
+            const storageRef = ref(storage, userName);
+
+            const uploadTask = uploadBytesResumable(storageRef, userFile);
+
+            // Register three observers:
+            // 1. 'state_changed' observer, called any time the state changes
+            // 2. Error observer, called on failure
+            // 3. Completion observer, called on successful completion
+            uploadTask.on('state_changed', 
+                (snapshot) => {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                        default:
+                            console.log('nothing la');
+                            break;
+                    }
+                }, 
+                (error) => {
+                    // Handle unsuccessful uploads
+                    setError(true);
+                }, 
+                () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+                        await updateProfile(result.user,{
+                            userName,
+                            photoURL: downloadURL,
+                        });
+                        await setDoc(doc(db, "users", result.user.uid),{
+                            uid: result.user.uid,
+                            userName,
+                            email,
+                            photoURL: downloadURL, 
+                        });
+                    });
+                }
+            );
+        }catch(err){
+            setError(true);
+        } 
+    }
     return (
         <div className='RegisterContainer'>
             <div className='RegisterWrpper'>
                 <span className='logo'>Chat Room </span>
                 <span className='title'>Register</span>
-                <form>
+                <form onSubmit={SumbitEvent}>
                     <input type='text' placeholder='name'/>
                     <input type='email' placeholder='email'/>
                     <input type='password' placeholder='password'/>
                     <input style={{display:'none'}} type='file' id ='file'/>
                     <label htmlFor='file'className="signupimage">
-                        <img  id = "addimage"src={AddImage} alt="add image"/>
+                        <img  id = "addimage" src={AddImage} alt="add"/>
                         <span>Add ur own image</span>
                     </label >
                     <button className='signup'>Sign Up</button>
                 </form>
+                {error && <span>Something Went Wrong</span>}
                 <p className='signup'>Already have an account? Login</p>
             </div>
         </div>
